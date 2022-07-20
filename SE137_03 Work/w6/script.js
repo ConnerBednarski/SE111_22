fc34dc31e682491e6281f03c050f9f42b67d7ef2
Conner B <cbednarski2021@gmail.com>
2022-07-20 11:47:40 -0400

    Test

---
+++ SE137_03 Work/w6/script.js
@@ -0,0 +1,24 @@
+var tray = document.querySelector(`#tray`);
+
+tray.addEventListener(`click`, openClose);
+
+function openClose(e)
+{
+    var nav = document.querySelector(`nav`);
+    nav.classList.toggle(`hidden`);
+}
+
+var tabs = document.querySelectorAll(`#tabs a`);
+console.log(tabs);
+
+for(let i = 0; i < tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, function(e){
+        for(let i = 0; i < tabs.length; i++)
+        {
+            tabs[i].style.backgroundColor=`orange`;
+        }
+        e.target.style.backgroundColor=`gray`;
+        document.querySelectorAll(`#breadcrumbs a`)[3].innerHTML = `Tabs ${i+1}`;
+    });
+}
